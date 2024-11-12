63a1f78c586272455a64778e5ec2326ecc156fc7
bloom256 <bloom256@gmail.com>
2024-11-12 18:15:01 +0400

    usdview running fix

---
+++ 3dInfoViewer/run_usdview.sh
@@ -0,0 +1,11 @@
+docker run -it --rm \
+--name 3d_info \
+--gpus all \
+--privileged \
+--env="DISPLAY" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--network host --ipc host \
+-e QT_X11_NO_MITSHM=1 \
+-v /:/hostfs \
+--entrypoint usdview \
+3d_info:dev "/hostfs/$1"
